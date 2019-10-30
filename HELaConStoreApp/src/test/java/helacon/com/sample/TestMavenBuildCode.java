d310e61ec53ae7ccbb54755837d126e248a14334
Hans-Erik Lange <hela@helacon.dk>
2019-10-30 21:08:12 +0100

    First commit code

---
+++ HELaConStoreApp/src/test/java/helacon/com/sample/TestMavenBuildCode.java
@@ -0,0 +1,18 @@
+package helacon.com.sample;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TestMavenBuildCode {
+    @Test
+    public void testHelloWorld() {
+        assertEquals("hello world", "hello world");
+    }
+    @Test
+    public void testHelloWorld2() {
+        assertEquals("hello world", "hello world");
+    }
+}
